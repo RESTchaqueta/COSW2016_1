9307fd6ab39cff7d456cef42c0c19f463f0f9099
Alternativacyhper <alternativacyhper@gmail.com>
2016-04-27 13:57:05 -0500

    v12

--- src/main/java/com/simulador/FormatoObjeto.java
+++ src/main/java/com/simulador/FormatoObjeto.java
@@ -8,10 +8,6 @@ package com.simulador;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
-/**
- *
- * @author usuario
- */
 public class FormatoObjeto extends ObjectMapper{
 
     public FormatoObjeto() {
